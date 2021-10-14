1a38144563555c8a1499769c1e75d911de148d4c
Hamza Ghelle <hamzaghellegmail.com>
2021-10-14 16:08:54 -0400

    new edits

--- 4_queries/avg_cohort_assistance_requests.sql
+++ 4_queries/avg_cohort_assistance_requests.sql
@@ -1,4 +1,4 @@
-SELECT cohorts.name as cohort, avg(max(completed_at) - max(started_at)) as average_assistance_time
+SELECT cohorts.name as cohort, avg(completed_at - started_at) as average_assistance_time
 FROM assistance_requests
 JOIN students ON students.id = assistance_requests.student_id
 JOIN cohorts ON cohorts.id = cohort_id
--- 4_queries/teachers_that_assisted.sql
+++ 4_queries/teachers_that_assisted.sql
@@ -1,8 +1,8 @@
-SELECT teachers.name as teacher, cohorts.name FROM (
-  SELECT cohorts.name 
-  FROM cohorts
-  WHERE cohorts.name = 'JUL02'
-) as cohort
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = 'JUL02'
 ORDER BY teacher;
 
